871c55b0f508887736c765fa7045d43c9bcdce9a
wensley033 <pierrewensley033@gmail.com>
2025-09-29 19:53:56 -0600

    Estructura inicial de microservicio

---
+++ src/main/java/mx/edu/uteq/idgs12/microservio_division/MicroservioDivisionApplication.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.microservio_division;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservioDivisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservioDivisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/edu/uteq/idgs12/microservio_division/MicroservioDivisionApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.edu.uteq.idgs12.microservio_division;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservioDivisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
